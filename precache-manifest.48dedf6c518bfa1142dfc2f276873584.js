a981b9235b5a25ed1eefaea8aa96c87d51dc6a36
ozubergz <jmin499@gmail.com>
2020-06-28 15:49:53 -0400

    Updates

---
+++ precache-manifest.48dedf6c518bfa1142dfc2f276873584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66fba3818ac6638fd31ab5b59fdb9b91",
+    "url": "/rap_app_frontend/index.html"
+  },
+  {
+    "revision": "6b93ffc3f459891ba945",
+    "url": "/rap_app_frontend/static/css/main.1a44ba34.chunk.css"
+  },
+  {
+    "revision": "c3b078dc3201be636d34",
+    "url": "/rap_app_frontend/static/js/2.55515ff0.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/rap_app_frontend/static/js/2.55515ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b93ffc3f459891ba945",
+    "url": "/rap_app_frontend/static/js/main.03694ea9.chunk.js"
+  },
+  {
+    "revision": "ccc15739bbbb449d6c19",
+    "url": "/rap_app_frontend/static/js/runtime-main.18683ec6.js"
+  },
+  {
+    "revision": "4788048f319dc48101678d9e69f5077e",
+    "url": "/rap_app_frontend/static/media/unknown_pic.4788048f.png"
+  }
+]);
